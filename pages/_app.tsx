6e62743586ca2e050fca8e5fbc09897d3cdf6210
Michael K Macharia <104422971+universal-projects@users.noreply.github.com>
2022-10-13 07:57:54 +0300

    Add files via upload

---
+++ pages/_app.tsx
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+`use strict` ;
+
+import `./../styles/globals.scss` ;
+
+export default function App ( { Component , pageProps } )
+{
+	return ( <Component {...pageProps} /> ) ;
+}
